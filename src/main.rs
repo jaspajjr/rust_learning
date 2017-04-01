e4de7a1995242a36ce75fdbc1afc5e8215c06a69
John Carney <jaspajr@gmail.com>
2017-04-01 18:04:02 +0100

    Added a function to take a command line input

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,32 @@
+use std::io;
+
+
 fn main() {
-    for i in 0..5{
-        println!("Hello {}", i);
-    }
+    let num = get_integer();
+    let array = build_array(32);
 }
 
+fn build_array(x: i32) -> Vec<i32>{
+    let mut array: Vec<i32> = Vec::with_capacity(x as usize);
+    for num in 0..x {
+        array.push(num);
+    }
+    return array;
+}
+
+
+fn get_integer() -> String{
+
+    println!("Please input the number.");
+
+    let mut num = String::new();
+
+    io::stdin().read_line(&mut num)
+        .expect("Failed to read input line.");
+
+        //let num i32: num.parse::<i32>(){
+        //    Ok(num) => num,
+        //    Err(error) => continue
+        //};
+    return num;
+}
